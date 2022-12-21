ed4b4a13c4b9c90a852c4798abcc456124c901a8
Kash Somani <kash@kashsomani.com>
2022-12-20 17:06:48 -0800

    failed to add daisy

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,21 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/routes/**/*.{svelte,js,ts}"],
-  plugins: [require("daisyui")],
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#CB6CE6",
+          secondary: "#5CE1E6",
+          accent: "#E5D0CC",
+          neutral: "#444554",
+          "base-100": "#172121",
+        },
+      },
+    ],
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
 }
